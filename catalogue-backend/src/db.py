46aad31686b244c62c55edfdc1a5018c22d31b7e
Kiran Joshi <kiran@swirrl.local>
2023-07-18 23:10:31 +0100

    Add poetry, pre-commit, docker, dotenv

--- catalogue-backend/src/db.py
+++ catalogue-backend/src/db.py
@@ -1,16 +1,33 @@
-from rdflib.namespace import DC, DCTERMS, DOAP, FOAF, SKOS, OWL, RDF, RDFS, VOID, XMLNS, XSD
+import os
+from rdflib.namespace import (
+    DC,
+    DCTERMS,
+    DOAP,
+    FOAF,
+    SKOS,
+    OWL,
+    RDF,
+    RDFS,
+    VOID,
+    XMLNS,
+    XSD,
+)
 from rdflib import Namespace
 from SPARQLWrapper import SPARQLWrapper, JSON, POST
 
-sparql_writer = SPARQLWrapper(
-    "http://localhost:3030/dev-triplestore/update"
-)
+from dotenv import load_dotenv
+
+load_dotenv()
+print(os.getenv("UPDATE_URL"))
+
+sparql_writer = SPARQLWrapper(os.environ.get("UPDATE_URL"))
 sparql_writer.setReturnFormat(JSON)
-sparql_writer.method=POST
+sparql_writer.method = POST
 
 MARKETPLACE = Namespace("http://dev.data-marketplace.gov.uk/")
 DATASET = Namespace(MARKETPLACE["dataset/"])
 catalogue_graph = MARKETPLACE["graph/catalogue"]
+
 
 def add_entry(ds_uri_slug, name, description):
     query = f"""
@@ -24,8 +41,11 @@ def add_entry(ds_uri_slug, name, description):
     ret = sparql_writer.queryAndConvert()
     return ret
 
-sparql_reader = SPARQLWrapper("http://localhost:3030/dev-triplestore/sparql")
+
+sparql_reader = SPARQLWrapper(os.environ.get("QUERY_URL"))
 sparql_reader.setReturnFormat(JSON)
+
+
 def list_datasets():
     query = f"""
     SELECT * WHERE {{
