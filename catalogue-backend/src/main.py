432a7663434c2bfcd0d0a172b405bc2e3355033e
madelinekosse <madelinekosse94@gmail.com>
2023-07-25 15:51:44 +0100

    Add organsation list endpoint
    And a little refactor

--- catalogue-backend/src/main.py
+++ catalogue-backend/src/main.py
@@ -2,14 +2,7 @@ from uuid import UUID
 from typing import Annotated, List
 from fastapi import FastAPI, Body, Query, HTTPException
 from fastapi.responses import JSONResponse
-from src.model import (
-    DataResource,
-    CreateResourceBody,
-    DataResourceSummary,
-    create,
-    resourceType,
-    organisationID,
-)
+import src.model as m
 from pydantic import BaseModel, field_validator
 
 app = FastAPI()
@@ -21,27 +14,34 @@ async def root():
 
 
 # TODO: in order to find out what filters are available, we need
-# and endpoint to return all of the available organisations, themes, and types
+# and endpoint to return all of the available organisations, themes, and types.
+# Something like this, except we'll need to figure out how themes work.
+# May not need it for types seeing as we only have 2
+
+
+@app.get("/list/organisations")
+async def list_organisations() -> List[m.Organisation]:
+    return [m.Organisation.parse_obj(o) for o in m.organisations.values()]
 
 
 # TODO: add theme query param
 @app.get("/catalogue")
 async def list_catalogue_entries(
     query: str = None,
-    publisherID: Annotated[List[organisationID], Query()] = [],
-    resourceType: Annotated[List[resourceType], Query()] = [],
+    publisherID: Annotated[List[m.organisationID], Query()] = [],
+    resourceType: Annotated[List[m.resourceType], Query()] = [],
     _limit: int = 100,
     _offset: int = 0,
-) -> list[DataResourceSummary]:
+) -> list[m.DataResourceSummary]:
     return []
 
 
 @app.get("/catalogue/{resource_id}")
-async def catalogue_entry_detail(resource_id: UUID) -> DataResource:
+async def catalogue_entry_detail(resource_id: UUID) -> m.DataResource:
     # Obviously, this would return the resource if it existed!
     raise HTTPException(status_code=404, detail="Item not found")
 
 
 @app.post("/dataset/new")
-async def new_ds(resource: CreateResourceBody) -> DataResource:
+async def new_ds(resource: m.CreateResourceBody) -> m.DataResource:
     return create(resource)
